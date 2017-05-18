a1963f7974fc2764409d86c38626b0068246b754
BrianRod <brod4910@gmail.com>
2017-05-17 22:40:23 -0600

    Now adds the use of hot module. Now switches between HtmlExtractPlugin when genereating production build and uses the hot module for dev build. uses cross-env for both windows and mac.

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,19 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+const isProd = process.argv.indexOf('-p') !== -1
+var cssDev = ['style-loader', 'css-loader'];
+var cssProd = ExtractTextPlugin.extract({
+    fallback: "style-loader",
+    use: "css-loader", //if using sass and css ['css-loader', 'sass-loader']
+    publicPath: "/dist"
+})
+
+var cssConfig = isProd ? cssProd : cssDev;
+
 module.exports = {
     entry: {
-        app: './src/app.js',
+        app: './src/index.js',
         contact: './src/contact.js'
     },
     output: {
@@ -16,17 +26,14 @@ module.exports = {
         contentBase: path.join(__dirname, "dist"),
         compress: true,
         stats: "errors-only",
+        hot: true,
         open: true
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader", //if using sass and css ['css-loader', 'sass-loader']
-                    publicPath: "/dist"
-                })
+                use: cssConfig
             },
             {
                 test: /\.js$/,
@@ -52,8 +59,10 @@ module.exports = {
         }),
         new ExtractTextPlugin({
             filename: 'app.css',
-            disable: false,
+            disable: !isProd,
             allChunks: true
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
     ]
 }
